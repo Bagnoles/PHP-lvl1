9ade5d93306e8b279489c4120e45fea080bfa37a
Anna <anna.mironova89@yandex.ru>
2021-10-07 18:04:52 +0300

    hw1

---
+++ hw1/task2.php
@@ -0,0 +1,9 @@
+<?php
+    $h1 = "Заголовок";
+    $title = "Название сайта";
+    $year = "2021";?>
+<html>
+<title><?php echo $title ?></title>
+<header><?php echo $h1 ?></header>
+<footer><?php echo $year ?></footer>
+</html>
