10552e057d8433fb5300437d2ebcc266ab1c06ca
Anna <anna.mironova89@yandex.ru>
2021-10-21 20:09:50 +0300

    В четвертый pull request случайно добавила не тот файл к домашнему заданию. Заметила только сейчас, исправляю.

--- hw4/task2.php
+++ hw4/task2.php
@@ -1,15 +1,15 @@
-<?php
-$images = scandir('/img');
-?>
 <html>
 <head>
 </head>
 <body>
 <?php
+$images = scandir('img');
 foreach ($images as $image) {
-    if (is_file('/images' . $image)) {
-        ?> <a href=<?php $image ?>target="_blank"><img src=<?php $image ?> width = "400"></a>
-    }
-}
+    if (is_file('img/' . $image)) {
+        ?> <a href="/img/<?php echo $image; ?>" target="_blank">
+            <img src="/img/<?php echo $image; ?>" width = "400">
+        </a>
+ <?php   }
+} ?>
 </body>
 </html>
