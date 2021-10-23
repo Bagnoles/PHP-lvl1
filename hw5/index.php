3e4335f0e721704774a23523165e7c295cba8198
Anna <anna.mironova89@yandex.ru>
2021-10-23 16:35:23 +0300

    hw5

---
+++ hw5/index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+</head>
+<body>
+<?php
+
+$images = scandir('img');
+foreach ($images as $image) {
+    if (is_file('img/' . $image)) {
+        ?> <a href="/img/<?php echo $image; ?>" target="_blank">
+            <img src="/img/<?php echo $image; ?>" width = "400">
+        </a>
+ <?php   }
+} ?>
+
+</body>
+</html>
